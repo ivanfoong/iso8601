7aa55f18535ed8dfff388d370d924f82f08bf181
Ivan Foong <ivan@d--buzz.com>
2014-11-01 11:06:31 +0800

    working json parsing for time format like "2014-01-23T12:36:00+08:00"

--- iso8601.go
+++ iso8601.go
@@ -7,38 +7,20 @@ import "time"
 const Format = "2006-01-02T15:04:05"
 const jsonFormat = `"` + Format + `"`
 
-var fixedZone = time.FixedZone("", 0)
-
 type Time time.Time
-
-// New constructs a new iso8601.Time instance from an existing
-// time.Time instance.  This causes the nanosecond field to be set to
-// 0, and its time zone set to a fixed zone with no offset from UTC
-// (but it is *not* UTC itself).
-func New(t time.Time) Time {
-	return Time(time.Date(
-		t.Year(),
-		t.Month(),
-		t.Day(),
-		t.Hour(),
-		t.Minute(),
-		t.Second(),
-		0,
-		fixedZone,
-	))
-}
 
 func (it Time) MarshalJSON() ([]byte, error) {
 	return []byte(time.Time(it).Format(jsonFormat)), nil
 }
 
 func (it *Time) UnmarshalJSON(data []byte) error {
-	t, err := time.ParseInLocation(jsonFormat, string(data), fixedZone)
-	if err == nil {
+	t, e := time.Parse(`"` + time.RFC3339 + `"`, string(data))
+
+	if e == nil {
 		*it = Time(t)
 	}
 
-	return err
+	return e;
 }
 
 func (it Time) String() string {
